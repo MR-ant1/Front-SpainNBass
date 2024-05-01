a923bdb8f5e3cba19c5c71f7ac6e88208cfa2e2c
antonio <antoniorc7@gmail.com>
2024-05-01 11:58:08 +0200

    funcionalidad: estructura basica de react-js y rutas creada

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { Body } from './pages/Body/Body'
+
+
+function App() {
+
+  return (
+    <>
+    <Body />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Body/Body.jsx
@@ -0,0 +1,15 @@
+
+import { Route, Routes} from 'react-router-dom';
+import { Register } from '../Register/Register';
+import { Home } from '../Home/Home';
+
+export const Body = () => {
+
+    return (
+        <Routes>
+            <Route path='/' element = {<Home />} />
+            <Route path='/register' element = {<Register />} />
+            
+        </Routes>
+    )
+}
