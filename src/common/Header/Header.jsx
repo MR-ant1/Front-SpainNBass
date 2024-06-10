2f3a045968ac1555f99d5ab0c7268050c1ea0756
antonio <antoniorc7@gmail.com>
2024-06-10 12:51:27 +0200

    fix: formato logo cambiado para tratar de mostrar en deploy

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -1,6 +1,6 @@
 
 import './Header.css'
-import logo from '../../../img/LogoSNB.png'
+import '../../../img/LogoSNB.png'
 import { Navigator } from '../Navigator/Navigator'
 import { useSelector, useDispatch } from "react-redux";
 import { userData, logout } from "../../app/Slices/userSlice";
@@ -38,7 +38,7 @@ export const Header = () => {
         <div className="headerDesign">
             <LogoNavigator
                 path="/"
-                title={logo}
+                title={<img src='../../../img/LogoSNB.jpg' alt="Logo"></img>}
             />
             {reduxUser.tokenData.token &&
                 <div className='dropDownContainer'>
